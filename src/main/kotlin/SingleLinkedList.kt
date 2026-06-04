7d96b5a20978d58dd2b92a8385352ac6597b1dbe
MariaPentegova <m.pentegova@yandex.ru>
2026-06-05 00:45:50 +0300

    Update SingleLinkedList.kt

--- src/main/kotlin/SingleLinkedList.kt
+++ src/main/kotlin/SingleLinkedList.kt
@@ -1,54 +1,117 @@
 package org.example
 
+import java.util.NoSuchElementException
+
 class SingleLinkedList : CustomList {
 
-    // don't use any java/kotlin internal datastructures like lists))
-    // write from scratch))
-
-
-    override val size: Int
-        get() = TODO("Implement this")
-
-    override fun add(element: Int) {
-        TODO("Implement this")
+    private class Item(var data: Int) {
+        var pointer: Item? = null
     }
 
-    override operator fun set(index: Int, value: Int) {
-        TODO("Implement this")
+    private var first: Item? = null
+    override var size: Int = 0
+
+    override fun add(element: Int) {
+        val newItem = Item(element)
+        if (first == null) {
+            first = newItem
+        } else {
+            var current = first
+            while (current?.pointer != null) {
+                current = current.pointer
+            }
+            current?.pointer = newItem
+        }
+        size++
     }
 
     override fun addFirst(element: Int) {
-        TODO("Implement this")
+        val newItem = Item(element)
+        newItem.pointer = first
+        first = newItem
+        size++
     }
 
     override operator fun get(index: Int): Int {
-        TODO("Implement this")
+        if (index !in 0 until size) {
+            throw IndexOutOfBoundsException("Index $index is out of range")
+        }
+        var current = first
+        var position = 0
+        while (position < index) {
+            current = current?.pointer
+            position++
+        }
+        return current?.data ?: throw NoSuchElementException()
+    }
+
+    override operator fun set(index: Int, value: Int) {
+        if (index !in 0 until size) {
+            throw IndexOutOfBoundsException("Index $index is out of range")
+        }
+        var current = first
+        var position = 0
+        while (position < index) {
+            current = current?.pointer
+            position++
+        }
+        current?.data = value
     }
 
     override fun indexOf(element: Int): Int {
-        TODO("Implement this")
+        var current = first
+        var position = 0
+        while (current != null) {
+            if (current.data == element) {
+                return position
+            }
+            current = current.pointer
+            position++
+        }
+        return -1
     }
 
     override fun remove(element: Int): Boolean {
-        TODO("Implement this")
+        // случай с удалением первого элемента
+        if (first?.data == element) {
+            first = first?.pointer
+            size--
+            return true
+        }
+        
+        var previous = first
+        while (previous?.pointer != null) {
+            if (previous.pointer?.data == element) {
+                previous.pointer = previous.pointer?.pointer
+                size--
+                return true
+            }
+            previous = previous.pointer
+        }
+        return false
     }
 
     override fun iterator(): Iterator<Int> {
         return object : Iterator<Int> {
-            override fun hasNext(): Boolean {
-                TODO("Implement this")
-            }
-
+            private var currentItem: Item? = first
+            
+            override fun hasNext(): Boolean = currentItem != null
+            
             override fun next(): Int {
-                TODO("Implement this")
+                val item = currentItem ?: throw NoSuchElementException()
+                currentItem = item.pointer
+                return item.data
             }
         }
     }
 
     companion object {
-        fun singleLinkedListOf(vararg items: Int) =
-            items.fold(SingleLinkedList()) { list, item ->
-                list.also{ it.add(item) }
+        fun singleLinkedListOf(vararg elements: Int): SingleLinkedList {
+            val list = SingleLinkedList()
+            for (element in elements) {
+                list.add(element)
             }
+            return list
+        }
     }
 }
